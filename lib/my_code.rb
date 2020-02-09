0b4ecb0eb7f76c6b7b7f687b3ac739b862da91b8
Michelle Frattaroli <mishyjari@outlook.com>
2020-02-09 00:03:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,33 @@
 # My Code here....
+def map_to_negative(arr)
+  i = 0
+  while i < arr.length do
+    arr[i] *= -1
+    i += i
+  end
+  arr
+end
+
+def map_to_no_change(arr)
+  arr
+end
+
+def map_to_double(arr)
+  i = 0
+  while i < arr.length do
+    arr[i] *= 2
+    i += 1
+  end
+  arr
+end
+
+def map_to_square(arr)
+  i = 0
+  while i < arr.length do
+    arr[i] *= arr[i]
+    i += 1
+  end
+  arr
+end
+
+def reduce_to_total
